c986228fab64296415da4d46775f681035e85268
Pavel Macík <pavel.macik@gmail.com>
2016-08-29 13:58:32 +0200

    Added a 'usage' output to printout all milestones with it's id.

--- github-issues/issues.groovy
+++ github-issues/issues.groovy
@@ -1,10 +1,21 @@
 import groovy.json.JsonSlurper;
 
-def release = args.length > 0 ? args[0] : 9;
+def json;
 
-def json = new JsonSlurper().parse(new URL("https://api.github.com/repos/PerfCake/PerfCake/issues?milestone=$release&state=closed&sort=created&direction=asc&labels=enhancement&per_page=1000"));
+if (args.length == 0) {
+   println "Usage: groovy issues.groovy <release_id>";
+   json = new JsonSlurper().parse(new URL("https://api.github.com/repos/PerfCake/PerfCake/milestones?state=all"));
+   json.forEach() {milestone ->
+      println(milestone.number + " = \"" + milestone.title + "\"");
+   }
+   return;
+}
+
+def release = args[0];
+
+json = new JsonSlurper().parse(new URL("https://api.github.com/repos/PerfCake/PerfCake/issues?milestone=$release&state=closed&sort=created&direction=asc&labels=enhancement&per_page=1000"));
 def enhancementFile = new File("release-$release-enhancements");
-enhancementFile.text="";
+enhancementFile.text = "";
 json.forEach() {issue ->
    if (issue.labels.find() {it -> it.name == "won't fix"} == null) {
       enhancementFile << " * " + issue.html_url + "\n";
