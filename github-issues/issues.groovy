716070c18e658c1025589855c65dfe33ba748db1
Pavel Macík <pavel.macik@gmail.com>
2016-08-29 16:46:13 +0200

    Updqted issues.groovy script

--- github-issues/issues.groovy
+++ github-issues/issues.groovy
@@ -2,9 +2,15 @@ import groovy.json.JsonSlurper;
 
 def json;
 
+/*/
+def acl="access_token=" + "place_github_api_access_token_here" + "&";
+/*/
+def acl=""
+//*/
+
 if (args.length == 0) {
    println "Usage: groovy issues.groovy <release_id>";
-   json = new JsonSlurper().parse(new URL("https://api.github.com/repos/PerfCake/PerfCake/milestones?state=all"));
+   json = new JsonSlurper().parse(new URL("https://api.github.com/repos/PerfCake/PerfCake/milestones?" + acl + "state=all"));
    json.forEach() {milestone ->
       println(milestone.number + " = \"" + milestone.title + "\"");
    }
@@ -13,23 +19,23 @@ if (args.length == 0) {
 
 def release = args[0];
 
-json = new JsonSlurper().parse(new URL("https://api.github.com/repos/PerfCake/PerfCake/issues?milestone=$release&state=closed&sort=created&direction=asc&labels=enhancement&per_page=1000"));
-def enhancementFile = new File("release-$release-enhancements");
-enhancementFile.text = "";
+json = new JsonSlurper().parse(new URL("https://api.github.com/repos/PerfCake/PerfCake/issues?" + acl + "milestone=$release&state=closed&sort=created&direction=asc&labels=enhancement&per_page=1000"));
+	println "Features:";
 json.forEach() {issue ->
    if (issue.labels.find() {it -> it.name == "won't fix"} == null) {
-      enhancementFile << " * " + issue.html_url + "\n";
-      enhancementFile << issue.title + "\n";
+      println " * " + issue.html_url;
+      println issue.title;
    }
 }
 
-json = new JsonSlurper().parse(new URL("https://api.github.com/repos/PerfCake/PerfCake/issues?milestone=$release&state=closed&sort=created&direction=asc&labels=bug&per_page=1000"));
-def bugFile = new File("release-$release-bugs");
+println "";
 
+json = new JsonSlurper().parse(new URL("https://api.github.com/repos/PerfCake/PerfCake/issues?" + acl + "milestone=$release&state=closed&sort=created&direction=asc&labels=bug&per_page=1000"));
+	println "Bug fixes:"
 json.forEach() {issue ->
    if (issue.labels.find() {it -> it.name == "won't fix"} == null) {
-      bugFile << " * " + issue.html_url + "\n";
-      bugFile << issue.title + "\n";
+      println " * " + issue.html_url ;
+      println issue.title;
    }
 }
 
